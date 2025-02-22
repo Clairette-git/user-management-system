33c7afe87ca2efc28d95fcef4ced7fa2b5e4238b
Clairette-git <marcletty12@gmail.com>
2025-02-22 22:33:59 +0200

    Fix DB connection issue

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-   
+  port: process.env.DB_PORT || 3306   
 
 });
 
